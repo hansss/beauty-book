66b57fefa16b6b5fc3f47649b1a9faed4269f030
hansss <hanakim@college.harvard.edu>
2014-07-01 11:00:22 +0100

    Fixed sign up spa form

--- app/models/salon.rb
+++ app/models/salon.rb
@@ -22,7 +22,7 @@ class Salon < ActiveRecord::Base
   validates :postal_code, presence: true
   validates :city, presence: true
   validates :phone, presence: true
-  validates :image, presence: true
+  validates :images, presence: true
 
   def sorted_phone_number
     Phoner::Phone.parse(phone)
